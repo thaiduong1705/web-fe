a79015abfde240b4d3883ed809fa9178db4e7776
Thai Duong <thaiduong7v@gmail.com>
2023-05-19 17:53:19 +0700

    chỉnh sửa chút

--- src/pages/Company/listcompanies.js
+++ src/pages/Company/listcompanies.js
@@ -4,10 +4,22 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { getCompanies } from '~/store/action/company';
+import ReactPaginate from 'react-paginate';
 const ListCompanies = () => {
     const companies = useSelector((state) => state.company.companies);
     const careers = useSelector((state) => state.otherData.careers);
     const [selectedCareer, setSelectedCareer] = useState();
+
+    const [pageNumber, setPageNumber] = useState(0);
+    const companyPerPage = 5;
+    const companyVisited = pageNumber * companyPerPage;
+    const pageCount = Math.ceil(companies.length / companyPerPage);
+    const displayCompany = companies.splice(companyVisited, companyVisited + companyPerPage).map((item) => {
+        return <CompanyItem key={item.id} item={item} />;
+    });
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
+    };
 
     const handleChangeCareer = (career) => {
         setSelectedCareer((prev) => career);
@@ -49,17 +61,27 @@ const ListCompanies = () => {
                 <div className="flex items-center">
                     <Link
                         to="/nha-tuyen-dung/tao-moi"
-                        className="bg-blue-600 text-white rounded-[8px] border-transparent border-1 flex items-center p-[8px] hover:opacity-80"
+                        className="bg-blue-600 text-white rounded-[8px] border-transparent border-1 flex items-center p-[8px] hover:opacity-80 px-3"
                     >
                         Thêm mới nhà tuyển dụng
                     </Link>
                 </div>
             </div>
 
-            <div>
-                {companies.map((company, index) => {
-                    return <CompanyItem key={company.id} item={company} />;
-                })}
+            <div className="">
+                {displayCompany}
+                <ReactPaginate
+                    pageCount={pageCount}
+                    previousLabel={'Prev'}
+                    nextLabel={'Next'}
+                    onPageChange={changePage}
+                    breakLabel={'...'}
+                    containerClassName="inline-flex -space-x-px items-center justify-center w-full"
+                    pageClassName={''}
+                    pageLinkClassName="px-[12px] py-[8px] leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    previousLinkClassName="block px-[12px] py-[8px] ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    nextLinkClassName="block px-[12px] py-[8px] ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                />
             </div>
         </div>
     );
