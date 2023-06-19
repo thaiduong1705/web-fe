e7341c69b94e5b50412267ccb238923453016ac7
Thai Duong <thaiduong7v@gmail.com>
2023-06-19 14:25:30 +0700

    chỉnh sửa layout thống kê

--- src/components/ReportCard.js
+++ src/components/ReportCard.js
@@ -1,24 +1,29 @@
 import React from 'react';
 
-const ReportCard = ({ number = 10, percent = 10, color = '3482F6' }) => {
+const ReportCard = ({ number = 10, percent = 10, color = '3482F6', content, className, icon }) => {
     return (
-        <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md w-[370px] h-[145px]">
-            <div
-                className={
-                    `absolute w-[64px] h-[64px] top-[-16px] left-[16px] overflow-hidden rounded-[12px] flex items-center justify-center text-white ` +
-                    `bg-[#${color}]`
-                }
-            >
-                icon
+        <div
+            className={
+                'flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md h-[145px] ' + className
+            }
+        >
+            <div className="flex items-center">
+                <div
+                    className={
+                        ` w-[64px] h-[64px] overflow-hidden rounded-[12px] flex items-center justify-center text-black text-[32px] ` +
+                        `bg-[#${color}]`
+                    }
+                >
+                    {icon}
+                </div>
+                <div className="p-[16px] flex flex-col">
+                    <p className="block antialiased leading-normal text-[14px] text-[#546e7a]">{content}</p>
+                    <h4 className="tracking-normal block antialiased text-[24px] font-semibold leading-snug text-black">
+                        {number}
+                    </h4>
+                </div>
             </div>
-            <div className="p-[16px] text-right">
-                <p className="block antialiased leading-normal text-[14px] text-[#546e7a]">
-                    Số lượng tuyển dụng hôm nay
-                </p>
-                <h4 className="tracking-normal block antialiased text-[24px] font-semibold leading-snug text-black">
-                    {number}
-                </h4>
-            </div>
+
             <div className="border-t border-[#ECEFF1] p-[16px]">
                 <strong className="text-green-400">+{percent}%</strong>
                 &nbsp; hơn tuần trước
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import clsx from 'clsx';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
-import { Stacked, Pie, Button, JobItem, ReportCard, BarChart, ChartCard, LineChart } from '~/components';
+import { ReportCard, BarChart, ChartCard, LineChart } from '~/components';
 
 import styles from './Homepage.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBuilding, faFile, faUsers } from '@fortawesome/free-solid-svg-icons';
 const Homepage = () => {
     const data = {
         labels: [...Array(7)].map((_, i) => {
@@ -24,33 +23,49 @@ const Homepage = () => {
         ],
     };
     return (
-        <div className="w-auto md:ml-96 h-screen">
-            <div className="">
-                <div className="grid gap-[24px] grid-cols-4 mx-[20px] my-[24px]">
-                    <ReportCard />
-                    <ReportCard />
-                    <ReportCard />
-                    <ReportCard />
-                </div>
-                <div className="my-[60px] mx-[20px] grid grid-cols-1 gap-y-[48px] gap-x-[24px] md:grid-cols-2 xl:grid-cols-3">
-                    <ChartCard context={'Số bài đăng trong 7 ngày vừa qua'}>
-                        <BarChart chartData={data} />
-                    </ChartCard>
-                    <ChartCard
-                        context={'Số lượt ứng tuyển trong 7 ngày vừa qua'}
-                        fromColor="from-pink-700"
-                        toColor="to-pink-400"
-                    >
-                        <LineChart chartData={data} />
-                    </ChartCard>
-                    <ChartCard
-                        context={'Số người ứng tuyển thành công trong 7 ngày vừa qua'}
-                        fromColor="from-green-700"
-                        toColor="to-green-400"
-                    >
-                        <LineChart chartData={data} />
-                    </ChartCard>
-                </div>
+        <div className="w-auto md:ml-96 h-full">
+            <div className="h-[150px] text-[32px] flex justify-center items-center bg-white rounded-[8px] font-medium">
+                Thống kê
+            </div>
+            <div className="grid gap-[24px] grid-cols-4 mx-[32px] my-[24px]">
+                <ReportCard content={'Số lượng công ty'} icon={<FontAwesomeIcon icon={faBuilding} />} />
+                <ReportCard content={'Số lượng ứng viên'} icon={<FontAwesomeIcon icon={faUsers} />} />
+                <ReportCard
+                    content={'Tổng tin đăng hiện tại'}
+                    className={'col-span-2'}
+                    icon={<FontAwesomeIcon icon={faFile} />}
+                />
+                <ReportCard content={'Số lượng tin đăng hôm nay'} icon={<FontAwesomeIcon icon={faFile} />} />
+                <ReportCard content={'Số lượng tin đăng còn hạn'} icon={<FontAwesomeIcon icon={faFile} />} />
+                <ReportCard content={'Số lượng tin đăng đã ẩn'} icon={<FontAwesomeIcon icon={faFile} />} />
+            </div>
+            <div className="my-[60px] mx-[20px] grid grid-cols-1 gap-y-[48px] gap-x-[24px] md:grid-cols-2 xl:grid-cols-3">
+                <ChartCard context={'Số bài đăng trong 7 ngày vừa qua'}>
+                    <BarChart chartData={data} />
+                </ChartCard>
+                <ChartCard
+                    context={'Số lượt ứng tuyển trong 7 ngày vừa qua'}
+                    fromColor="from-pink-700"
+                    toColor="to-pink-400"
+                >
+                    <LineChart chartData={data} />
+                </ChartCard>
+                <ChartCard
+                    context={'Số người ứng tuyển thành công trong 7 ngày vừa qua'}
+                    fromColor="from-green-700"
+                    toColor="to-green-400"
+                >
+                    <LineChart chartData={data} />
+                </ChartCard>
+            </div>
+
+            <div className="my-[28px] mx-[32px]">
+                <span>Top các lĩnh vực có bài tuyển dụng nhiều nhất</span>
+                <div>Top1:</div>
+                <div>Top2:</div>
+                <div>Top3:</div>
+                <div>Top4:</div>
+                <div>Top5:</div>
             </div>
         </div>
     );
