142f02a14639bca4105b3d9a5d2ec818aabf2f7f
Thai Duong <thaiduong7v@gmail.com>
2023-06-23 21:42:27 +0700

    sửa sidebar 1 chút

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,11 +7,12 @@ import { privateRoutes } from '~/routes';
 import { useStateContext } from '~/contexts/Context';
 
 import styles from './Sidebar.module.css';
+import SubNav from './SubNav';
 const Sidebar = () => {
     const activeLink =
         'flex items-center gap-5 pl-[16px] pt-[12px] pb-[12px] pr-[16px] rounded-[8px] text-white font-medium text-md m-2 bg-blue-600';
     const normalLink =
-        'flex items-center gap-5 pl-[16px] pt-[12px] pb-[12px] pr-[16px] rounded-[8px] text-md text-blue-600 font-medium dark:text-gray-200 hover:text-white hover:bg-blue-400 m-2 hover:transition-all';
+        'flex items-center gap-5 pl-[16px] pt-[12px] pb-[12px] pr-[16px] rounded-[8px] text-md text-blue-600 font-medium dark:text-gray-200 hover:text-white hover:bg-blue-700 m-2 hover:transition-all';
     const { activeMenu, setActiveMenu } = useStateContext();
     return (
         <div className="ml-4 mt-4 rounded-[8px] h-[960px] w-[260px] bg-white p-3 fixed">
@@ -29,19 +30,7 @@ const Sidebar = () => {
                     </div>
                     <div className="mt-6">
                         {privateRoutes.map((route, index) => {
-                            return (
-                                <NavLink
-                                    to={route.path}
-                                    key={index}
-                                    className={({ isActive }) => (isActive ? activeLink : normalLink) + ' flex-1'}
-                                    onClick={() => {
-                                        setActiveMenu((prev) => true);
-                                    }}
-                                >
-                                    {route.icon}
-                                    <span className="capitalize">{route.title}</span>
-                                </NavLink>
-                            );
+                            return <SubNav item={route} />;
                         })}
                     </div>
                 </>
